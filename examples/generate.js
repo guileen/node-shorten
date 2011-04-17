8c8ceabedc5ec45caf89f35303e157a433e12e81
guilin 桂林 <guileen@gmail.com>
2011-04-17 11:18:09 +0800

    add id32

--- examples/generate.js
+++ examples/generate.js
@@ -3,26 +3,38 @@ var Shorten = require('../lib');
 var redisClient = redis.createClient();
 var shorten = new Shorten(redisClient);
 
-for (var i = 0; i < 1000; i++){
+for (var i = 0; i < 20; i++){
   shorten.nextId(function(err, id){
       console.log(id);
-    });
+  });
 }
 
-for (var i = 0; i < 1000; i++){
-  shorten.nextIdEx(function(err, id){
+for (var i = 0; i < 20; i++){
+  shorten.nextId32(function(err, id){
       console.log(id);
-    });
+  });
 }
 
-for (var i = 0; i < 1000; i++){
+for (var i = 0; i < 20; i++){
+  shorten.nextId64(function(err, id){
+      console.log(id);
+  });
+}
+
+for (var i = 0; i < 10; i++){
   shorten.nextId('topic', function(err, id){
       console.log(id);
-    });
+  });
 }
 
-for (var i = 0; i < 1000; i++){
-  shorten.nextIdEx('topic', function(err, id){
+for (var i = 0; i < 10; i++){
+  shorten.nextId32('topic', function(err, id){
       console.log(id);
-    });
+  });
+}
+
+for (var i = 0; i < 10; i++){
+  shorten.nextId64('topic', function(err, id){
+      console.log(id);
+  });
 }
